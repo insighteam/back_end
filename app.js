d9af1087682cb21bbd95ee4bb5ecdbe4a8ad896f
Sunghee <630sunghee@gmail.com>
2019-08-02 03:45:14 +0900

    Update capsules/new router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -12,6 +13,9 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
